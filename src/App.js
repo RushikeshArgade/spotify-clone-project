35c56e15c2e14003b4e42c19ce24b8f98853ead5
RushikeshArgade <93438031+RushikeshArgade@users.noreply.github.com>
2022-01-07 21:45:28 +0530

    Completed with basic look alike
    created web app as Spotify but it does not have all the functionality.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,52 @@
-import React,{useState, useEffect} from 'react';
-import './App.css';
-import Login from './Login';
-import { getTokenFromUrl } from './spotify';
-import SpotifyWebApi from 'spotify-web-api-js';
+import React, { useEffect, useState } from "react";
+import Login from "./Login";
+import "./App.css";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
 
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState();
+  const [{ user, token }, dispatch] = useDataLayerValue();
 
-  useEffect(()=>{
+  useEffect(() => {
     const hash = getTokenFromUrl();
-    window.location.hash ="";
+    window.location.hash = "";
     const _token = hash.access_token;
-
-    if(_token){
-      setToken(_token);
+    if (_token) {
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+      console.log("[token]", token);
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user,
+        });
+      });
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists,
+        });
+      });
+      spotify.play();
+      spotify.getPlaylist("27ca11eda12f4e3e9c4bf6987160e5bf").then((playlist) => {
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: playlist,
+        });
+      });
     }
-
-    console.log("token",token);
-
-  },[]);
+  }, []);
 
   return (
     <div className="App">
-        {token ? <h1>Logged in</h1> : <Login />}
+      {token ? <Player spotify={spotify} /> : <Login />}
     </div>
   );
 }
--- src/Player.js
+++ src/Player.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from "react";
+import "./Player.css";
+import Sidebar from "./Sidebar";
+import Body from "./Body";
+import Footer from "./Footer";
 
-function Player() {
-    return (
-        <div>
-            <h1>We are in Player</h1>
-        </div>
-    )
+function Player({ spotify }) {
+  return (
+    <div className="player">
+      <div className="player__body">
+        <Sidebar />
+        <Body spotify={spotify} />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
-export default Player
-
-
-import react from 'react'
+export default Player;
